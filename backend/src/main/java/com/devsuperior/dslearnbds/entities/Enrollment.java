0e123a93106db0e55b2297b4a45df0c6fe8b946e
pinhobrunodev <iaishowcs@gmail.com>
2021-10-23 21:28:12 -0300

    Fix Timestamp

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslearnbds.entities;
 
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
 
+import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -16,7 +17,9 @@ public class Enrollment implements Serializable {
 
     @EmbeddedId
     private EnrollmentPK id = new EnrollmentPK();
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant enrollMoment;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant refundMoment;
     private boolean available;
     private boolean onlyUpdate;
